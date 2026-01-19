64843460da528d3ec1af932b73ac1f8d3ac6865a
viniciusP23 <158086630+viniciusP23@users.noreply.github.com>
2026-01-19 19:55:03 -0300

    lógica pt 17

--- exercicios/contador/script.js
+++ exercicios/contador/script.js
@@ -1,24 +1,63 @@
-let contador = 0
+const semafaro = document.getElementById("semafaro")
+const text = document.getElementById("text")
+let estado = "verde"
 
-function atualizarTabela() {
-    const valor = document.getElementById("valor")
-    valor.innerText = contador
-}
+semafaro.addEventListener("click", () => {
+    
+    if(estado === "verde") {
+        semafaro.style.background = "yellow"
+        text.innerHTML = "o semáforo esta AMARELO"
+        estado = "amarelo"
+    } else if(estado === "amarelo") {
+        semafaro.style.background = "red"
+        text.innerHTML = "o semáforo esta VERMELHO"
+        estado = "vermelho"
+    } else {
+        semafaro.style.background = "green"
+        text.innerHTML = "o semáforo esta VERDE"
+        estado = "verde"
+    }
+})
+
+
+//////
+
+
+const input = document.getElementById("senha")
+const botao = document.getElementById("toggle")
+let visivel = false
+
+botao.addEventListener("click", () => {
+
+    if(visivel === false) {
+        input.type = "text"
+        botao.innerHTML = "Mostrar"
+        visivel = true
+    }else {
+        input.type = "password"
+        botao.innerHTML = "Esconder"
+        visivel = false
+    }
+})
+
+//
+
+const result = document.getElementById("result")
+let contador = 0
 
 function somar() {
     if(contador < 10) {
-        contador++
-        atualizarTabela()
-    }
-
-    if(contador === 10) {
+        contador ++
+        result.innerHTML = contador
+    } else{
         alert("passou de 10")
     }
+
 }
 
 function subtrair() {
-    if(contador > 0) {
-        contador--
-        atualizarTabela()
+    if(contador > 0 ) {
+        contador --
+        result.innerHTML = contador
     }
 }
