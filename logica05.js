b5e3d70338706bd8c2f8a005a632fce1932110e2
viniciusP23 <158086630+viniciusP23@users.noreply.github.com>
2026-03-09 11:06:17 -0300

    lógica pt 33

--- logica05.js
+++ logica05.js
@@ -301,3 +301,310 @@ for(let i = 0; i < numerosSegundoMaior.length; i++) {
 }
 
 console.log(segundoM)
+
+///
+
+const qunatosPares = [4, 7, 2, 9, 6]
+
+let paresQuantos = 0
+
+for(let i = 0; i < qunatosPares.length; i++) {
+
+    const num = qunatosPares[i]
+
+    if(num % 2 === 0) {
+        paresQuantos++
+    }
+}
+
+console.log(paresQuantos)
+
+///
+
+const somarTodos = [3, 8, 5, 10, 7]
+
+let somas = 0
+
+for(let i = 0; i < somarTodos.length; i++) {
+    somas += somarTodos[i]
+}
+
+console.log(somas)
+
+///
+
+const QuantosMaiorQ10 = [12, 5, 18, 3, 20]
+
+let maioresNum10 = 0
+
+for(let i = 0; i < QuantosMaiorQ10.length; i++) {
+
+    const num = QuantosMaiorQ10[i]
+
+    if(num > 10) {
+        maioresNum10++
+    }
+}
+
+console.log(maioresNum10)
+
+///
+
+const maiorNum = [8, 3, 15, 6, 2]
+
+let maiorNumero1 = maiorNum[0]
+
+for(let i = 0; i < maiorNum.length; i++) {
+
+    const num = maiorNum[i]
+
+    if(num > maiorNumero1) {
+        maiorNumero1 = num
+    }
+}
+
+console.log(maiorNumero1)
+
+///
+
+const menorNum = [9, 4, 7, 1, 6]
+
+let menorNumero1 = menorNum[0]
+
+for(let i = 0; i < menorNum.length; i++) {
+
+    const num = menorNum[i]
+
+    if(num < menorNumero1) {
+        menorNumero1 = num
+    }
+}
+
+console.log(menorNumero1)
+
+///
+
+const maiorNumPar = [7, 12, 4, 19, 8, 3]
+
+let maiorNumeroPar = maiorNumPar[0]
+
+for(let i = 0; i < maiorNumPar.length; i++) {
+
+    const num = maiorNumPar[i]
+
+    if(num % 2 === 0) {
+
+        if(num > maiorNumeroPar) {
+            maiorNumeroPar = num
+        }
+    }
+}
+
+console.log(maiorNumeroPar)
+
+///
+
+
+const maioresNum = [5, 12, 18, 7, 21, 14]
+
+let primeiroNumMaior = -Infinity
+let segundoNumMaior = -Infinity
+
+for(let i = 0; i < maioresNum.length; i++) {
+
+    const num = maioresNum[i]
+
+    if(num > primeiroNumMaior) {
+        segundoNumMaior = primeiroNumMaior
+        primeiroNumMaior = num
+    }else if(num > segundoNumMaior) {
+        segundoNumMaior = num
+    }
+
+}
+
+console.log(primeiroNumMaior)
+console.log(segundoNumMaior)
+
+///
+
+const segundoMaiorPar = [3, 9, 2, 15, 8, 7]
+
+let primeiroPar = -Infinity
+let segundoPar = -Infinity
+
+for(let i = 0; i < segundoMaiorPar.length; i++) {
+
+    const num = segundoMaiorPar[i]
+
+    if(num % 2 === 0) {
+
+        if(num > primeiroPar) {
+            segundoPar = primeiroPar
+            primeiroPar = num
+        }else if(num > segundoPar) {
+            segundoPar = num
+        }
+
+    }
+}
+
+console.log(segundoPar)
+
+///
+
+const segundoMaiorQ10 = [11, 4, 17, 6, 9, 20]
+
+let primeiroNumMaior1 = -Infinity
+let segundoNumMaior1 = -Infinity
+
+for(let i = 0; i < segundoMaiorQ10.length; i++) {
+
+    const num = segundoMaiorQ10[i]
+
+    if(num > 10) {
+
+        if(num > primeiroNumMaior1) {
+            segundoNumMaior1 = primeiroNumMaior1
+            primeiroNumMaior1 = num
+        }else if(num > segundoNumMaior1) {
+            segundoNumMaior1 = num
+        }
+    }
+}
+
+console.log(segundoNumMaior1)
+
+///
+
+
+const todosAoQuadrado = [1, 2, 3, 4, 5]
+
+const aoQuadrados = []
+
+for(let i = 0; i < todosAoQuadrado.length; i++) {
+
+    const num = todosAoQuadrado[i]
+
+    aoQuadrados.push(num ** 2)
+}
+
+console.log(aoQuadrados)
+
+///
+
+const apenasNumPares = [3, 6, 1, 8, 5]
+
+const paresNum = []
+
+for(let i = 0; i < apenasNumPares.length; i++) {
+
+    const num = apenasNumPares[i]
+
+    if(num % 2 === 0) {
+        paresNum.push(num)
+    }
+}
+
+console.log(paresNum)
+
+///
+
+const imparesMultiplicados = [2, 5, 7, 4, 9]
+
+const imparMult = []
+
+for(let i = 0; i < imparesMultiplicados.length; i++) {
+
+    const num = imparesMultiplicados[i]
+
+    if(num % 2 === 1) {
+
+        imparMult.push(num * 3)
+    }
+}
+
+console.log(imparMult)
+
+///
+
+const maiorParQ10 = [2, 17, 9, 24, 13, 30, 5]
+
+let maiorPar10 = -Infinity
+
+for(let i = 0; i < maiorParQ10.length; i++) {
+
+    const num = maiorParQ10[i]
+
+    if(num % 2 === 0) {
+
+        if(num > 10) {
+
+            if(num > maiorPar10) {
+                maiorPar10 = num
+            }
+        }
+    }
+}
+
+console.log(maiorPar10)
+
+///
+
+const segundoMaiorNumPar = [4, 12, 7, 20, 3, 15, 8]
+
+let priPar = -Infinity
+let segPar = -Infinity
+
+for(let i = 0; i < segundoMaiorNumPar.length; i++) {
+
+    const num = segundoMaiorNumPar[i]
+
+    if(num % 2 === 0) {
+
+        if(num > priPar) {
+            segPar = priPar
+            priPar = num
+        }
+    }
+}
+
+console.log(segPar)
+
+///
+
+
+const oitoAoQuadrado = [3, 14, 7, 18, 5, 10]
+
+let oitoQuadrado = []
+
+for(let i = 0; i < oitoAoQuadrado.length; i++) {
+
+    const num = oitoAoQuadrado[i]
+
+    if(num > 8) {
+        oitoQuadrado.push(num ** 2)
+    }
+    
+
+}
+
+console.log(oitoQuadrado)
+
+///
+
+const letras = ["a", "b", "a", "c", "b", "a"]
+
+const frequencia = {}
+
+for(let i = 0; i < letras.length; i++) {
+
+    const letra = letras[i]
+
+    if(frequencia[letra]) {
+        frequencia[letra]++
+    }else {
+        frequencia[letra] = 1
+    }
+}
+console.log(frequencia)
