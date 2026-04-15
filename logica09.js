e38f9f06d3ea5a96eb9c76811ae5795bb88959de
viniciusP23 <vinicius_pereira1@live.com>
2026-04-14 22:52:24 -0300

    lógica pt 45

--- logica09.js
+++ logica09.js
@@ -109,3 +109,118 @@ console.log(total)
 
 // 
 
+const precos = [10, 20, 30, 15]
+
+let totalSomar = 0
+
+for(let i = 0; i < precos.length; i++) {
+
+    const num = precos[i]
+
+    totalSomar += num
+}
+
+console.log(totalSomar)
+
+// 
+
+const carrinhoCompras = [
+  { nome: "Camiseta", preco: 50, qtd: 2 },
+  { nome: "Tênis", preco: 200, qtd: 1 }
+]
+
+let total01 = 0
+let maiorValor = carrinhoCompras[0]
+
+for(let item of carrinhoCompras) {
+
+    total01 += item.preco * item.qtd
+
+    if(item.preco > maiorValor.preco) {
+        maiorValor = item
+    }
+}
+
+console.log(total01)
+console.log(maiorValor)
+
+// 
+
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome
+        this.preco = preco
+    }
+
+    mostrarProduto() {
+        return `Produto: ${this.nome} - ${this.preco}`
+    }
+
+    aplicarDesconto(porcentagem){
+        return this.preco = this.preco - (this.preco * (porcentagem / 100))
+    }
+
+    aumetarPreco(porcentagem) {
+        return this.preco = this.preco + (this.preco * (porcentagem / 100))
+    }
+}
+
+const p1 = new Produto("Camiseta", 50)
+
+console.log(p1.mostrarProduto())
+
+p1.aplicarDesconto(10)
+console.log("desconto:", p1.mostrarProduto())
+
+p1.aumetarPreco(10)
+console.log("aumento:", p1.mostrarProduto())
+
+// 
+
+
+class Mercadorias {
+    constructor(nome, preco) {
+        this.nome = nome
+        this.preco = preco
+    }
+
+    mostrarProduto() {
+        return `Produtos: ${this.nome} - ${this.preco}`
+    }
+}
+
+
+class Carrinho {
+    constructor() {
+        this.itens = []
+    }
+
+    adicionarMercadoria(produto) {
+        this.itens.push(produto)
+    }
+
+    removerProduto(nome) {
+        this.itens = this.itens.filter(item => item.nome !== nome)
+    }
+
+    calcularTotal() {
+
+        return this.itens.reduce((total, item) => {
+            return total + item.preco
+        }, 0)
+    }
+}
+
+const cart = new Carrinho()
+
+const m1 = new Mercadorias("Camiseta", 100)
+const m2 = new Mercadorias("meia", 50)
+
+cart.adicionarMercadoria(m1)
+cart.adicionarMercadoria(m2)
+
+console.log("Total:", cart.calcularTotal())
+
+cart.removerProduto("Camiseta")
+
+console.log("Removidos", cart.calcularTotal())
