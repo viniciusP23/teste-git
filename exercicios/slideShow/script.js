d564ad1ce0def53a7ba3a053354aadfa2c641927
viniciusP23 <vinicius_pereira1@live.com>
2026-05-15 18:14:42 -0300

    lógica pt 53

---
+++ exercicios/slideShow/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const images = [
+    { 'id': '1', 'url':'./img/chrono.jpg' },
+    { 'id': '2', 'url':'./img/inuyasha.jpg' },
+    { 'id': '3', 'url':'./img/tenchi.jpg' },
+    { 'id': '4', 'url':'./img/tenjhotenge.jpg' },
+    { 'id': '5', 'url':'./img/yuyuhakusho.jpg' },
+    { 'id': '6', 'url':'./img/ippo.png' },
+]
+
+const containerItems = document.querySelector("#container-items")
+
+
+const loandImages = (images, container) => {
+
+    images.forEach(image => {
+        container.innerHTML += `
+        
+        <div class="item">
+        
+            <img src="${image.url}"
+        
+        </div>
+    `
+    });
+}
+
+loandImages(images, containerItems)
+
+let items = document.querySelectorAll(".item")
+
+const previous = () => {
+    containerItems.appendChild(items[0])
+    items = document.querySelectorAll(".item")
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1]
+    containerItems.insertBefore(lastItem, items[0])
+}
+
+document.querySelector("#previous").addEventListener("click", previous)
+document.querySelector("#next").addEventListener("click", previous)
