3823ccc8ff3c28d88dafae3fdc33ad0c2fa47e0c
viniciusP23 <vinicius_pereira1@live.com>
2026-07-22 22:33:25 -0300

    lógica pt 62

--- POO/Classes/classe.js
+++ POO/Classes/classe.js
@@ -29,7 +29,7 @@ class Carro {
     freiar() {
         this.velocidade -= 10
 
-        if(this.velocidade < 0) {
+        if (this.velocidade < 0) {
             this.velocidade = 0
         }
     }
@@ -63,7 +63,7 @@ class ContaBancaria {
 
     sacar(valor) {
 
-        if(valor > this.saldo) {
+        if (valor > this.saldo) {
             console.log("Saldo insuficiente")
             return
         }
@@ -94,7 +94,7 @@ class ProdutosLoja {
 
     vender(qtd) {
 
-        if(qtd === 0) {
+        if (qtd === 0) {
             return console.log("produto indisponivel no estoque")
         }
 
@@ -120,7 +120,7 @@ console.log(produto1.mostrarInfo())
 //////////
 
 class NotaAlunos {
-    constructor(nome, notas= []) {
+    constructor(nome, notas = []) {
         this.nome = nome
         this.notas = notas
     }
@@ -132,7 +132,7 @@ class NotaAlunos {
     media() {
         let soma = 0
 
-        for(let nota of this.notas) {
+        for (let nota of this.notas) {
             soma += nota
         }
 
@@ -141,9 +141,9 @@ class NotaAlunos {
 
     situacao() {
 
-        if(this.media() >= 7) {
+        if (this.media() >= 7) {
             return "Aprovado"
-        }else {
+        } else {
             return "Reprovado"
         }
     }
@@ -258,3 +258,206 @@ biblioteca.devolverLivro("O Hobbit")
 
 biblioteca.emprestarLivro("1984")
 
+/////////
+
+
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome
+        this.preco = preco
+    }
+}
+
+class Carrinho {
+    constructor() {
+        this.listaProdutos = []
+    }
+
+
+    adicionarProduto(produto) {
+        this.listaProdutos.push(produto)
+    }
+
+    removerProduto(nome) {
+
+        const remover = this.listaProdutos.find(produto => produto.nome === nome)
+
+        if (!remover) {
+            return "Produto não encontrado!"
+        }
+
+        this.listaProdutos = this.listaProdutos.filter(
+            produto => produto.nome !== nome
+        )
+
+        return "Produto removido!"
+    }
+
+    total() {
+        let somar = 0
+
+        for (let produto of this.listaProdutos) {
+            somar += produto.preco
+        }
+
+        return somar
+    }
+}
+
+const carrinho = new Carrinho()
+
+carrinho.adicionarProduto(
+    new Produto("arroz", 12.99)
+)
+
+carrinho.adicionarProduto(
+    new Produto("feijão", 10.99)
+)
+
+carrinho.adicionarProduto(
+    new Produto("banana", 5.99)
+)
+
+carrinho.removerProduto("arroz")
+
+console.log(carrinho.total())
+
+////////
+
+class Animal {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    comer() {
+        console.log(`O ${this.nome} está comendo sua ração`)
+    }
+
+    dormir() {
+        console.log(`O ${this.nome} está dormindo em sua casinha`)
+    }
+}
+
+class Cachorro extends Animal {
+
+}
+
+const rex = new Cachorro("Rex")
+
+rex.dormir()
+rex.comer()
+
+//////////////
+
+class Funcioonario {
+    constructor(nome, salario) {
+        this.nome = nome
+        this.salario = salario
+    }
+
+    trabalhar() {
+        console.log(`${this.nome} está trabalahndo.`)
+    }
+}
+
+class Programador extends Funcioonario {
+    constructor(nome, salario, linguagem) {
+        super(nome, salario)
+        this.linguagem = linguagem
+    }
+
+    trabalhar() {
+        console.log(`${this.nome} está progrmando em ${this.linguagem}`)
+    }
+}
+
+const dev = new Programador("vinicius", 5000, "JavaScript")
+
+dev.trabalhar()
+
+
+///////
+
+class Veiculo {
+    constructor(marca, modelo) {
+        this.marca = marca
+        this.modelo = modelo
+    }
+
+    descricao() {
+
+        console.log(`A linha ${this.marca} tem os melhores de ${this.modelo}`)
+
+    }
+}
+
+class Moto extends Veiculo {
+    constructor(marca, modelo, cilindrada) {
+        super(marca, modelo)
+        this.cilindrada = cilindrada
+    }
+
+    descricao() {
+        super.descricao()
+        console.log(`Cilindrada: ${this.cilindrada} cc`)
+    }
+}
+
+const bmw = new Moto("BMW", "GS", 310)
+
+bmw.descricao()
+
+/////////
+class Conta {
+    constructor(titular, saldo) {
+        this.titular = titular
+        this.saldo = saldo
+    }
+
+    depositar(valor) {
+        this.saldo += valor
+    }
+
+    sacar(valor) {
+        if (this.saldo < valor) {
+            console.log("Saldo insuficiente.")
+            return
+        }
+
+        this.saldo -= valor
+    }
+
+    mostrarSaldo() {
+        console.log(`Saldo: ${this.saldo}`)
+    }
+}
+
+class ContaCorrente extends Conta {
+
+    sacar(valor) {
+        const taxa = 5
+
+        if (valor + taxa > this.saldo) {
+            console.log("Saldo insuficiente.")
+            return
+        }
+
+        this.saldo -= valor + taxa
+    }
+}
+
+class ContaPoupanca extends Conta {
+    renderJuros() {
+        const juros = this.saldo * 0.5
+
+        this.saldo += juros
+
+        console.log(`Juros adicionados: R$ ${juros}`)
+    }
+}
+
+const cc = new ContaCorrente("vinicius", 1000)
+
+cc.sacar(100)
+
+cc.mostrarSaldo()
